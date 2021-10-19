ed5e6ca5130eeacfeb86275c56bf8298db6b91f2
JIYEON_OnTime <54522206+Ji-Yeon0107@users.noreply.github.com>
2021-10-20 02:25:44 +0900

    Add files via upload

--- pages/Result.js
+++ pages/Result.js
@@ -1,12 +1,29 @@
 import React, {useState} from 'react'
 import { useRouter } from "next/router";
+import Head from './Head'
 
 const Result = () => {
     const router = useRouter();
+    
+    const test = () => {
+
+        Kakao.Link.createCustomButton({
+  container: '.kakao-link-btn',
+  templateId: 63843,
+  templateArgs: {
+    'title': '제목 영역입니다.',
+    'description': '설명 영역입니다.'
+  }
+});
+
+    }
+
+    
   
     return (
       <div>
-        <p>결과입니당</p>
+          <Head />
+        <p className="kakao-link-btn" onClick={test}>결과입니당</p>
     </div>
     );
   }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import Link from "next/link";
 import React, {useState} from 'react'
 import {Questions} from '../components/Questions'
+import Head from './Head'
 
 const Home = () => {
 
     const [start, setStart]= useState(false);
     const [score, setScore] = useState(0);
 
+
+
     return(
         <div>
+            <Head></Head>
+           
             <p>스우파 과몰입꾼 테스트</p>
             {
                 start
